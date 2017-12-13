a6ba8d8bdbc8798c448ab4dcc14fd9cdf07d43d9
Joel Handwell <joelhandwell@gmail.com>
2017-12-13 13:55:42 -0500

    Update php 7.1 to 7.2

--- setup.sh
+++ setup.sh
@@ -2,7 +2,10 @@
 sleep 30
 sudo apt update
 sudo apt install -y apache2
-sudo apt install -y php7.1 php7.1-mysql php7.1-curl php7.1-json libapache2-mod-php7.1
+sudo apt-get install -y python-software-properties
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt-get update -y
+sudo apt install -y php7.2 php7.2-mysql php7.2-curl php7.2-json libapache2-mod-php7.2
 sudo -E debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 sudo -E debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 sudo apt-get -y install mysql-server mysql-client
